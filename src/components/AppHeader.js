9171dcc416c5f70473752f95433de754f036154d
Xica Felix <xicasfelix@gmail.com>
2023-01-15 13:57:42 -0500

    changing appHeader styling

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import { Header, Icon } from "semantic-ui-react";
 
 function AppHeader(){
     return(
-        <Header as= 'h1' textAlign='center' size="large" style={{color: 'whitesmoke'}}> <span style={{color: 'whitesmoke'}}> <Icon circular size="small" color="white" name="linkify" /></span> Mockchain </Header>
+        <Header as= 'h1' textAlign='center' size="large" inverted> <span> <Icon circular size="small" color="white" inverted name="linkify" /></span> Mockchain </Header>
     );
 }
 
